0a355f118a9df8f7cb824db338cfd1c66ed8739e
viqifirdaus <viqifirdaus_95@yahoo.com>
2016-12-11 12:24:59 +0700

    setting find a ride and offer a ride with firebase

--- www/js/controllers/FindARideCtrl.js
+++ www/js/controllers/FindARideCtrl.js
@@ -1,4 +1,11 @@
-app.controller('FindARideCtrl', function ($scope, $stateParams, ionicMaterialInk) {
+app.controller('FindARideCtrl', function ($scope, $state, $stateParams, ionicMaterialInk, CONFIG, REFERENCE, $firebaseArray, sharedProperties) {
     //ionic.material.ink.displayEffect();
     ionicMaterialInk.displayEffect();
+
+    $scope.form = { from: '', to: '', seat: 0 };
+
+    $scope.clickFormButton = function () {
+        sharedProperties.setFormFindARide($scope.form);
+        $state.go('app.find-a-ride-search-result');
+    }
 });
--- www/js/controllers/FindARideSearchResultCtrl.js
+++ www/js/controllers/FindARideSearchResultCtrl.js
@@ -1,4 +1,43 @@
-app.controller('FindARideSearchResultCtrl', function ($scope, $stateParams, ionicMaterialInk) {
+app.controller('FindARideSearchResultCtrl', function ($scope, $stateParams, ionicMaterialInk, CONFIG, REFERENCE, $firebaseArray, sharedProperties) {
     //ionic.material.ink.displayEffect();
     ionicMaterialInk.displayEffect();
+    var filterRide = sharedProperties.getFormFindARide()
+    var database = firebase.database();
+
+    // get list hari
+    var offers = firebase.database().ref(REFERENCE.OFFER);
+    var users = firebase.database().ref(REFERENCE.USER);
+
+    console.log(filterRide);
+
+    offers.orderByChild('city_from').equalTo(filterRide.from.toLowerCase()).on('value', function(snapshot) {
+    	$scope.offers = [];
+    	var results = snapshot.val();
+    	var i = 0;
+
+        angular.forEach(results, function(value, key) {        	
+        	if(value['city_to'].toLowerCase() == filterRide.to.toLowerCase()){
+	        	var userId = value['user_id'];
+        	
+	        	users.child(userId).on('value', function(snapshot) {
+					var user = snapshot.val();
+
+					results[key]['name'] = user['nama_depan'] + ' ' + user['nama_balakang'];
+					results[key]['age'] = new Date().getFullYear() - user['tahun_lahir'];
+				});
+
+				$scope.offers[i++] = results[key];
+			}
+		});
+		console.log(results);
+    });
+
+    // offers.on('child_changed', function(data) {
+    //     console.log(data);
+    //     updateHari(postElement, data.key, data.nama, data);
+    // });
+
+    // offers.on('child_removed', function(data) {
+    //     deleteHari(postElement, data.key);
+    // });
 });
